c925bbf10848a2216ba30c39e6f049ca91fbf02b
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-15 18:59:28 -0400

    Actualizacion de postBooksController.js

--- src/server/controllers/postBooksController.js
+++ src/server/controllers/postBooksController.js
@@ -2,9 +2,9 @@ import db from '../config/db.js';
 
 export const createBook = async (book) => {
   try {
-    const query = 'INSERT INTO books (title, author, edition) VALUES (?, ?, ?';
-    const { name, author, edition } = book;
-    const result = await db.execute(query, [name, author, edition]);
+    const query = 'INSERT INTO books (title, author, edition) VALUES (?, ?, ?)';
+    const { title, author, edition } = book;
+    const result = await db.execute(query, [title, author, edition]);
 
     return result;
   } catch (error) {
