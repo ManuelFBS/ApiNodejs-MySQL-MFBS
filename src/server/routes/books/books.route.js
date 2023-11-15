ffa091d18d44371c9eb8aecad2be392f5f422f64
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-15 18:57:05 -0400

    Actualizacion de books.route.js

--- src/server/routes/books/books.route.js
+++ src/server/routes/books/books.route.js
@@ -3,9 +3,12 @@ import {
   getAllBooksHandler,
   getBookByIDHandler
 } from '../../handlers/getBooksHandler.js';
+import { createBookHandler } from '../../handlers/postBooksHandler.js';
 
 export const bookRoutes = Router();
 
 bookRoutes.get('/', getAllBooksHandler);
 
 bookRoutes.get('/byid/:id', getBookByIDHandler);
+
+bookRoutes.post('/create', createBookHandler);
