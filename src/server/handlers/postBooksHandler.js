6f0a55d47c41ef66f29a707e227866fe85d136f0
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-15 18:50:21 -0400

    Actualizacion de postBooksHandler.js

---
+++ src/server/handlers/postBooksHandler.js
@@ -0,0 +1,20 @@
+import * as bookPOST from '../controllers/postBooksController.js';
+
+export const createBookHandler = async (req, res) => {
+  try {
+    const book = req.body;
+    const result = await bookPOST.createBook(book);
+
+    res.status(201).json({
+      message: 'The book has been successfully registered...!!!',
+      data: result[0]
+    });
+
+    return result;
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error...',
+      error: error.message
+    });
+  }
+};
