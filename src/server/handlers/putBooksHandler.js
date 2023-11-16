fa803b73d240d2954673a0c4758280f8d09a1030
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-16 00:36:55 -0400

    Actualizacion de putBooksHandler.js

---
+++ src/server/handlers/putBooksHandler.js
@@ -0,0 +1,18 @@
+import { updateBook } from '../controllers/putBooksController';
+
+export const updateBookHandler = async (req, res) => {
+  const { id } = req.params;
+  const updatedFieldsBook = req.body;
+  try {
+    const result = await updateBook(id, updatedFieldsBook);
+
+    res.status(200).json({
+      messsage: 'The book has been updated...!!!',
+      result
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Error when trying to update the book...!!!'
+    });
+  }
+};
