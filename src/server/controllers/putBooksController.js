1535f71394a31a17c110a3d451cea3c5ecf52556
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-16 01:34:33 -0400

    Actualizando putBooksController.js

--- src/server/controllers/putBooksController.js
+++ src/server/controllers/putBooksController.js
@@ -25,24 +25,3 @@ export const updateBook = async (id, book) => {
     throw error;
   }
 };
-/*
-export const updateBook = async (id, book) => {
-  try {
-    const fieldsToUpdate = [];
-    const valuesToUpdate = [];
-
-    for (const key in book) {
-      fieldsToUpdate.push(`${key} = ?`);
-      valuesToUpdate.push(book[key]);
-    }
-
-    const query = `UPDATE books SET ${fieldsToUpdate.join(', ')} WHERE id = ?`;
-    const bookUpdate = await db.execute(query, [...valuesToUpdate, id]);
-
-    return bookUpdate;
-  } catch (error) {
-    throw error;
-  }
-};
-
-*/
