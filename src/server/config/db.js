657b2c2c2c0cca306614e140a8e29c4d49d2b090
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-17 15:05:00 -0400

    Comentando db.js

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/* El código está creando un objeto de conexión para conectarse a una base de datos MySQL. Utiliza el
+método `mysql.createConnection()` para crear el objeto de conexión y pasa un objeto con los detalles
+de conexión necesarios. Los detalles de la conexión se recuperan de las variables de entorno
+utilizando `process.env`. Las variables de entorno `DB_HOST`, `DB_USER`, `DB_PASSWORD`, `DB_PORT` y
+`DB_NAME` se utilizan para especificar el host, el usuario, la contraseña, el puerto y el nombre de
+la base de datos, respectivamente. */
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
